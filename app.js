7fc3c1025dce07740f277f9e1a73f45798a1784d
Matthew Baer <matt.baer77@gmail.com>
2023-05-24 23:00:08 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Models
+
+// Game
+// Init
+// Shuffle
+// Draw Card
+
+// App
+// Select Button
+// Init on load
+// Shuffle on load
+// Button draws card
+// Create card html
+// Put card on page
+
