974d3192a335ab8cc8a557c71db1a84c637d3ea0
Matthew Baer <matt.baer77@gmail.com>
2023-05-25 23:39:19 -0400

    completed

--- app2.js
+++ app2.js
@@ -1,7 +1,7 @@
 // Global Variables
 const draw_button = document.querySelector('.draw-button');
 const cardArea = document.querySelector('.card-area');
-const baseURL = 'https://deckofcardsapi.com/api/deck';
+const cardsBaseURL = 'https://deckofcardsapi.com/api/deck';
 
 // Create Card HTML
 function createCardHTML(imgSrc, offset) {
@@ -21,9 +21,9 @@ function createCardHTML(imgSrc, offset) {
 window.addEventListener("load", (e) => {
 
     // 1
-    axios.get(`${baseURL}/new/shuffle`)
+    axios.get(`${cardsBaseURL}/new/shuffle`)
     .then(res => {
-        return axios.get(`${baseURL}/${res.data.deck_id}/draw`)
+        return axios.get(`${cardsBaseURL}/${res.data.deck_id}/draw`)
     })
     .then(res => {
         console.log(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`)
@@ -36,14 +36,14 @@ window.addEventListener("load", (e) => {
     let cardTwoString;
 
     // 2 Call
-    axios.get(`${baseURL}/new/shuffle`)
+    axios.get(`${cardsBaseURL}/new/shuffle`)
     .then(res => {
         deckId2 = res.data.deck_id
-        return axios.get(`${baseURL}/${deckId2}/draw`)
+        return axios.get(`${cardsBaseURL}/${deckId2}/draw`)
     })
     .then(res => {
         cardOneString = `${res.data.cards[0].value} of ${res.data.cards[0].suit}`
-        return axios.get(`${baseURL}/${deckId2}/draw`)
+        return axios.get(`${cardsBaseURL}/${deckId2}/draw`)
     })
     .then(res => {
         cardTwoString = `${res.data.cards[0].value} of ${res.data.cards[0].suit}`
@@ -56,7 +56,7 @@ window.addEventListener("load", (e) => {
     let previousOffset = 0;
 
     // Get Deck ID - Show draw button.
-    axios.get(`${baseURL}/new/shuffle`)
+    axios.get(`${cardsBaseURL}/new/shuffle`)
     .then(res => {
         deck_id = res.data.deck_id;
         draw_button.style.display = "block"
@@ -64,7 +64,7 @@ window.addEventListener("load", (e) => {
 
     // Event Listener for button click.
     draw_button.addEventListener('click', (e) => {
-        axios.get(`${baseURL}/${deck_id}/draw/`).then(({data}) => {
+        axios.get(`${cardsBaseURL}/${deck_id}/draw/`).then(({data}) => {
 
             let imgSrc = data.cards[0].image;
             let offset = previousOffset + 60;
